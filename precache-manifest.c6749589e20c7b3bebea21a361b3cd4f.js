3a741f7c6e44d5e23c65e5c9ba7ab16aec238001
Avinesh Singh <avin3sh@gmail.com>
2019-03-04 22:05:49 +0530

    Updates

---
+++ precache-manifest.c6749589e20c7b3bebea21a361b3cd4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d29d16a4f8f722e406bc",
+    "url": "/firebase-react-bookmark-manager/static/js/runtime~main.d29d16a4.js"
+  },
+  {
+    "revision": "79854a9b7ecd1b20ab6b",
+    "url": "/firebase-react-bookmark-manager/static/js/main.79854a9b.chunk.js"
+  },
+  {
+    "revision": "4ad46fffbb455d52e5df",
+    "url": "/firebase-react-bookmark-manager/static/js/2.4ad46fff.chunk.js"
+  },
+  {
+    "revision": "79854a9b7ecd1b20ab6b",
+    "url": "/firebase-react-bookmark-manager/static/css/main.e60d37bc.chunk.css"
+  },
+  {
+    "revision": "6a1de0849b5e8256e793094445afe291",
+    "url": "/firebase-react-bookmark-manager/index.html"
+  }
+];
